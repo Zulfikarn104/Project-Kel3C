2ea7776e01854200360884d72aef446aa9928f24
Zulfikarn104 <zulfikarnaim104@gmail.com>
2024-07-02 10:17:47 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,10 +15,6 @@ udara ambien di lokasi tertentu dan didasarkan kepada dampak terhadap kesehatan 
 Perhitungan ISPU dilakukan berdasarkan nilai ISPU batas atas, ISPU batas bawah, ambient
 batas atas, ambient batas bawah, dan konsentrasi ambient hasil pengukuran. Untuk mengecek kondisi udaramu bisa dibawah ini yaa..""")
 
-with st.expander('***MORE INFORMATION***'):
-    st.caption('Pengertian')
-    st.success('Limbah gas merupakan hasil buangan yang menggunakan udara sebagai medianya dan udara secara alami  mengandung unsur-unsur kimia seperti O2, N2, NO2, CO2, dan H2.')
-
 
 #PERHITUNGAN ISPU
 st.write("""# YUK! CEK KONDISI UDARA MU!
@@ -49,10 +45,51 @@ if Submit:
 
 with st.expander('***MORE INFORMATION***'):
     st.caption('KATEGORI ISPU (Indeks Standar Pencemar Udara)')
-    st.success('1-50 Baik; 51-100 Sedang; 101-200 Tidak Sehat; 201-300 Sangat Tidak Sehat')
+    st.info('1-50 Baik; 51-100 Sedang; 101-200 Tidak Sehat; 201-300 Sangat Tidak Sehat')
 
 #ARTIKEL
+st.write("""
+         # PENGERTIAN DAN JENIS LIMBAH GAS
+Limbah gas merupakan hasil buangan yang menggunakan udara sebagai medianya dan udara secara alami  mengandung unsur-unsur kimia seperti O2, N2, NO2, CO2, dan H2.
 
+Zat pencemar udara dibagi menjadi dua bagian: partikel dan gas, berikut :
+
+**A. Partikel** : partikel kecil dan dapat dilihat dengan mata telanjang.
+ Contoh: uap air, debu, asap, kabut, kabut.
+
+**B. Gas** : hanya dapat dirasakan melalui baunya (gas tertentu) atau akibat langsungnya.
+ 	
+**Materi Partikulat** : Limbah gas yang dilepaskan ke udara biasanya mengandung partikel bahan padat atau cair yang  sangat kecil dan ringan yang tersuspensi dalam gas. Contoh : gas yang dihasilkan dari pabrik berupa asap, partikel, dan debu.
+""")
+st.write("""
+         # PENYEBAB PENCEMARAN UDARA""")
+a = '''**1. Aktivitas Pertambangan :**
+aktivitas pertambangan dapat menghasilkan debu dan bahan kimia lainnya yang akhirnya dilepaskan ke udara. hal tersebut dilakukan untuk mengekstraksi mineral yang ada di bawah bumi menggunakan alat besar yang menggunakan bahan bakar untuk menggerakkannya.
+
+**2. aktivitas pertanian :** 
+para petani sering menggunakan pupuk kimia yang dapat mempengaruhi proses panen mereka dan ketika melakukan kegiatan pembakaran lahan maka gas ataupun asap yang dikeluarkan akan lebih berbahaya dan menimbulkan pencemaran udara.
+
+**3. penggunaan listrik yang berlebihan :**
+hal ini dikarenakan dalam membuat listrik di indonesia masih mengandalkan bahan bakar batu bara. oleh sebab itu, semakin banyaknya penggunaan listrik maka limbah batu bara yang akan dibuang ke udara akan semakin banyak dan menimbulkan pencemaran udara.
+
+**4. asap yang dihasilkan pabrik :**
+pada sebuah pabrik biasanya asap tersebut dibuang melalui cerobong asap besar dalam jumlah yang besar. asap yang dikeluarkan industri pabrik merupakan salah satu penyumbang terbesar gas karbon yang ada di udara. namun bukan hanya mencemari udara tetapi juga menimbulkan terjadinya pemanasan global.
+
+**5.limbah yang dihasilkan rumah tangga :**
+kegiatan rumah tangga seringkali menghasilkan sampah. sampah tersebut kemudian dibuang dan dibakar sehingga menghasilkan asap yang membuat munculnya pencemaran udara. 
+'''
+st.markdown(a)
+
+st.write(''' # JADI.. SOLUSINYA APA??
+Untuk menangani hal tersebut kamu bisa loh dengan mengganti kebiasaan mu... Seperti,
+###### 1. Jalan kaki dan megendarai sepeda
+###### 2. Bercocok tanam 
+###### 3. Tidak membakar sampah
+###### 4. Menghemat penggunaan listrik 
+tapi masih banyak lagi lohh.. hal yang bisa kamu lakuin untuk mengurangi pencemaran ini. ''')
+
+st.write(""" # KESIMPULAN
+limbah gas merupakan isu lingkungan yang signifikan yang perlu diperhatikan dengan serius. limbah gas industri, transportasi, dan pertanian memiliki dampak negatif terhadap kualitas udara, tanah, air, dan ekosistem. manajemen limbah gas adalah tanggung jawab bersama untuk generasi saat ini dan masa depan kita. """)
 
 #SIDEBAR
 with st.sidebar:
@@ -64,4 +101,16 @@ with st.sidebar:
         st.info('SELAMAT DATANG DI WEB KAMI. Web ini dibuat untuk pemenuhan Tugas Akhir Mata Kuliah Pemrograman Dasar Semester 2 Studi Teknik Pengolahan Limbah. Web ini berisi mengenai Informasi serta Edukasi terhadap pencemaran udara. Harapan Besar kami dengan adanya Website ini dapat menambah wawasan serta kepekaan terhadap Pencermaran Udara. ')
 
     with st.expander('**FITUR**'):
-        st.success('Dalam Web ini anda dapat mengetahui info seputar Pencemaran Udara serta Dapat Menghitung secara otomatis Indeks Udara ISPU dengan parameternya.')
+        st.warning('Dalam Web ini anda dapat mengetahui info seputar Pencemaran Udara serta Dapat Menghitung secara otomatis Indeks Udara ISPU dengan parameternya.')
+
+    with st.expander('**DAFTAR ISI**'):
+        st.success('''1. Latar Belakang
+2. Perhitungan ISPU
+3. Pengertian dan Jenis Limbah Gas
+4. Penyebab Pencemaran Udara
+5. Solusi Pencemaran Udara
+6. Kesimpulan 
+                   ''')
+    st.caption('*Develop by* **MOCH ZULFIKAR NAIM**')
+    st.caption('*Support by* **SHOFWATUL QOLBIA & ERLINA MUSTAFA**')
+
